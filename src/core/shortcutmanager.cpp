aa02966e8d78b63b9da6d98e2cfd5921cdd48c76
ZhiFen Dev <dev@zhifen.local>
2026-08-30 15:06:28 +0800

    修复: QKeySequence显式构造

--- src/core/shortcutmanager.cpp
+++ src/core/shortcutmanager.cpp
@@ -56,75 +56,75 @@ void ShortcutManager::loadDefaults()
 
     // 视图
     registerShortcut("view.zoom", "缩放", Qt::Key_Z, SC_View);
-    registerShortcut("view.zoom.extents", "全部缩放", "Ctrl+Shift+E", SC_View);
-    registerShortcut("view.zoom.window", "窗口缩放", "Ctrl+W", SC_View);
-    registerShortcut("view.zoom.in", "放大", "Ctrl++", SC_View);
-    registerShortcut("view.zoom.out", "缩小", "Ctrl+-", SC_View);
+    registerShortcut("view.zoom.extents", "全部缩放", QKeySequence("Ctrl+Shift+E"), SC_View);
+    registerShortcut("view.zoom.window", "窗口缩放", QKeySequence("Ctrl+W"), SC_View);
+    registerShortcut("view.zoom.in", "放大", QKeySequence("Ctrl++"), SC_View);
+    registerShortcut("view.zoom.out", "缩小", QKeySequence("Ctrl+-"), SC_View);
     registerShortcut("view.pan", "平移", Qt::Key_P, SC_View);
-    registerShortcut("view.regen", "重生成", "RE", SC_View);
-    registerShortcut("view.redraw", "重画", "R", SC_View);
+    registerShortcut("view.regen", "重生成", QKeySequence("RE"), SC_View);
+    registerShortcut("view.redraw", "重画", QKeySequence("R"), SC_View);
 
     // 绘图
     registerShortcut("draw.line", "直线", Qt::Key_L, SC_Draw);
     registerShortcut("draw.circle", "圆", Qt::Key_C, SC_Draw);
     registerShortcut("draw.arc", "圆弧", Qt::Key_A, SC_Draw);
-    registerShortcut("draw.rectangle", "矩形", "REC", SC_Draw);
-    registerShortcut("draw.polyline", "多段线", "PL", SC_Draw);
-    registerShortcut("draw.polygon", "多边形", "POL", SC_Draw);
-    registerShortcut("draw.ellipse", "椭圆", "EL", SC_Draw);
-    registerShortcut("draw.text", "文字", "DT", SC_Draw);
-    registerShortcut("draw.mtext", "多行文字", "MT", SC_Draw);
-    registerShortcut("draw.hatch", "图案填充", "H", SC_Draw);
-    registerShortcut("draw.block", "创建块", "B", SC_Draw);
-    registerShortcut("draw.insert", "插入块", "I", SC_Draw);
-    registerShortcut("draw.point", "点", "PO", SC_Draw);
+    registerShortcut("draw.rectangle", "矩形", QKeySequence("REC"), SC_Draw);
+    registerShortcut("draw.polyline", "多段线", QKeySequence("PL"), SC_Draw);
+    registerShortcut("draw.polygon", "多边形", QKeySequence("POL"), SC_Draw);
+    registerShortcut("draw.ellipse", "椭圆", QKeySequence("EL"), SC_Draw);
+    registerShortcut("draw.text", "文字", QKeySequence("DT"), SC_Draw);
+    registerShortcut("draw.mtext", "多行文字", QKeySequence("MT"), SC_Draw);
+    registerShortcut("draw.hatch", "图案填充", QKeySequence("H"), SC_Draw);
+    registerShortcut("draw.block", "创建块", QKeySequence("B"), SC_Draw);
+    registerShortcut("draw.insert", "插入块", QKeySequence("I"), SC_Draw);
+    registerShortcut("draw.point", "点", QKeySequence("PO"), SC_Draw);
 
     // 修改
     registerShortcut("modify.move", "移动", Qt::Key_M, SC_Modify);
-    registerShortcut("modify.copy", "复制", "CO", SC_Modify);
-    registerShortcut("modify.rotate", "旋转", "RO", SC_Modify);
-    registerShortcut("modify.scale", "缩放", "SC", SC_Modify);
-    registerShortcut("modify.mirror", "镜像", "MI", SC_Modify);
-    registerShortcut("modify.offset", "偏移", "O", SC_Modify);
-    registerShortcut("modify.trim", "修剪", "TR", SC_Modify);
-    registerShortcut("modify.extend", "延伸", "EX", SC_Modify);
-    registerShortcut("modify.break", "打断", "BR", SC_Modify);
-    registerShortcut("modify.fillet", "圆角", "F", SC_Modify);
-    registerShortcut("modify.chamfer", "倒角", "CHA", SC_Modify);
-    registerShortcut("modify.explode", "分解", "X", SC_Modify);
-    registerShortcut("modify.array", "阵列", "AR", SC_Modify);
-    registerShortcut("modify.stretch", "拉伸", "S", SC_Modify);
-    registerShortcut("modify.lengthen", "拉长", "LEN", SC_Modify);
-    registerShortcut("modify.join", "合并", "J", SC_Modify);
+    registerShortcut("modify.copy", "复制", QKeySequence("CO"), SC_Modify);
+    registerShortcut("modify.rotate", "旋转", QKeySequence("RO"), SC_Modify);
+    registerShortcut("modify.scale", "缩放", QKeySequence("SC"), SC_Modify);
+    registerShortcut("modify.mirror", "镜像", QKeySequence("MI"), SC_Modify);
+    registerShortcut("modify.offset", "偏移", QKeySequence("O"), SC_Modify);
+    registerShortcut("modify.trim", "修剪", QKeySequence("TR"), SC_Modify);
+    registerShortcut("modify.extend", "延伸", QKeySequence("EX"), SC_Modify);
+    registerShortcut("modify.break", "打断", QKeySequence("BR"), SC_Modify);
+    registerShortcut("modify.fillet", "圆角", QKeySequence("F"), SC_Modify);
+    registerShortcut("modify.chamfer", "倒角", QKeySequence("CHA"), SC_Modify);
+    registerShortcut("modify.explode", "分解", QKeySequence("X"), SC_Modify);
+    registerShortcut("modify.array", "阵列", QKeySequence("AR"), SC_Modify);
+    registerShortcut("modify.stretch", "拉伸", QKeySequence("S"), SC_Modify);
+    registerShortcut("modify.lengthen", "拉长", QKeySequence("LEN"), SC_Modify);
+    registerShortcut("modify.join", "合并", QKeySequence("J"), SC_Modify);
 
     // 标注
-    registerShortcut("dim.linear", "线性标注", "DLI", SC_Dimension);
-    registerShortcut("dim.aligned", "对齐标注", "DAL", SC_Dimension);
-    registerShortcut("dim.radius", "半径标注", "DRA", SC_Dimension);
-    registerShortcut("dim.diameter", "直径标注", "DDI", SC_Dimension);
-    registerShortcut("dim.angular", "角度标注", "DAN", SC_Dimension);
-    registerShortcut("dim.baseline", "基线标注", "DBA", SC_Dimension);
-    registerShortcut("dim.continue", "连续标注", "DCO", SC_Dimension);
-    registerShortcut("dim.leader", "引线标注", "LE", SC_Dimension);
-    registerShortcut("dim.tolerance", "公差", "TOL", SC_Dimension);
-    registerShortcut("dim.center", "圆心标记", "DCE", SC_Dimension);
-    registerShortcut("dim.edit", "编辑标注", "DED", SC_Dimension);
-    registerShortcut("dim.style", "标注样式", "D", SC_Dimension);
+    registerShortcut("dim.linear", "线性标注", QKeySequence("DLI"), SC_Dimension);
+    registerShortcut("dim.aligned", "对齐标注", QKeySequence("DAL"), SC_Dimension);
+    registerShortcut("dim.radius", "半径标注", QKeySequence("DRA"), SC_Dimension);
+    registerShortcut("dim.diameter", "直径标注", QKeySequence("DDI"), SC_Dimension);
+    registerShortcut("dim.angular", "角度标注", QKeySequence("DAN"), SC_Dimension);
+    registerShortcut("dim.baseline", "基线标注", QKeySequence("DBA"), SC_Dimension);
+    registerShortcut("dim.continue", "连续标注", QKeySequence("DCO"), SC_Dimension);
+    registerShortcut("dim.leader", "引线标注", QKeySequence("LE"), SC_Dimension);
+    registerShortcut("dim.tolerance", "公差", QKeySequence("TOL"), SC_Dimension);
+    registerShortcut("dim.center", "圆心标记", QKeySequence("DCE"), SC_Dimension);
+    registerShortcut("dim.edit", "编辑标注", QKeySequence("DED"), SC_Dimension);
+    registerShortcut("dim.style", "标注样式", QKeySequence("D"), SC_Dimension);
 
     // 图层
     registerShortcut("layer.manager", "图层特性", Qt::Key_F2, SC_Layer);
-    registerShortcut("layer.make", "将对象的图层置为当前", "MAC", SC_Layer);
-    registerShortcut("layer.previous", "上一个图层", "LAYERP", SC_Layer);
-    registerShortcut("layer.on", "打开图层", "LAYON", SC_Layer);
-    registerShortcut("layer.off", "关闭图层", "LAYOFF", SC_Layer);
-    registerShortcut("layer.freeze", "冻结图层", "LAYFRZ", SC_Layer);
-    registerShortcut("layer.thaw", "解冻图层", "LAYTHW", SC_Layer);
-    registerShortcut("layer.lock", "锁定图层", "LAYLCK", SC_Layer);
-    registerShortcut("layer.unlock", "解锁图层", "LAYULK", SC_Layer);
-    registerShortcut("layer.isolate", "隔离图层", "LAYISO", SC_Layer);
-    registerShortcut("layer.unisolate", "取消隔离", "LAYUNISO", SC_Layer);
-    registerShortcut("layer.match", "图层匹配", "LAYMCH", SC_Layer);
-    registerShortcut("layer.copy", "将对象复制到新图层", "COPYTOLAYER", SC_Layer);
+    registerShortcut("layer.make", "将对象的图层置为当前", QKeySequence("MAC"), SC_Layer);
+    registerShortcut("layer.previous", "上一个图层", QKeySequence("LAYERP"), SC_Layer);
+    registerShortcut("layer.on", "打开图层", QKeySequence("LAYON"), SC_Layer);
+    registerShortcut("layer.off", "关闭图层", QKeySequence("LAYOFF"), SC_Layer);
+    registerShortcut("layer.freeze", "冻结图层", QKeySequence("LAYFRZ"), SC_Layer);
+    registerShortcut("layer.thaw", "解冻图层", QKeySequence("LAYTHW"), SC_Layer);
+    registerShortcut("layer.lock", "锁定图层", QKeySequence("LAYLCK"), SC_Layer);
+    registerShortcut("layer.unlock", "解锁图层", QKeySequence("LAYULK"), SC_Layer);
+    registerShortcut("layer.isolate", "隔离图层", QKeySequence("LAYISO"), SC_Layer);
+    registerShortcut("layer.unisolate", "取消隔离", QKeySequence("LAYUNISO"), SC_Layer);
+    registerShortcut("layer.match", "图层匹配", QKeySequence("LAYMCH"), SC_Layer);
+    registerShortcut("layer.copy", "将对象复制到新图层", QKeySequence("COPYTOLAYER"), SC_Layer);
 
     // 工具
     registerShortcut("tools.grid", "网格", Qt::Key_F7, SC_Tools);
@@ -133,24 +133,24 @@ void ShortcutManager::loadDefaults()
     registerShortcut("tools.polar", "极轴追踪", Qt::Key_F10, SC_Tools);
     registerShortcut("tools.otrack", "对象追踪", Qt::Key_F11, SC_Tools);
     registerShortcut("tools.dyninput", "动态输入", Qt::Key_F12, SC_Tools);
-    registerShortcut("tools.lwt", "线宽显示", "LWT", SC_Tools);
-    registerShortcut("tools.ucs", "UCS", "UC", SC_Tools);
-    registerShortcut("tools.dsettings", "草图设置", "DS", SC_Tools);
-    registerShortcut("tools.options", "选项", "OP", SC_Tools);
-    registerShortcut("tools.matchprop", "特性匹配", "MA", SC_Tools);
-    registerShortcut("tools.properties", "特性", "PR", SC_Tools);
-    registerShortcut("tools.qselect", "快速选择", "QSELECT", SC_Tools);
+    registerShortcut("tools.lwt", "线宽显示", QKeySequence("LWT"), SC_Tools);
+    registerShortcut("tools.ucs", "UCS", QKeySequence("UC"), SC_Tools);
+    registerShortcut("tools.dsettings", "草图设置", QKeySequence("DS"), SC_Tools);
+    registerShortcut("tools.options", "选项", QKeySequence("OP"), SC_Tools);
+    registerShortcut("tools.matchprop", "特性匹配", QKeySequence("MA"), SC_Tools);
+    registerShortcut("tools.properties", "特性", QKeySequence("PR"), SC_Tools);
+    registerShortcut("tools.qselect", "快速选择", QKeySequence("QSELECT"), SC_Tools);
 
     // 窗口
-    registerShortcut("window.cascade", "层叠", "Ctrl+Shift+C", SC_Window);
-    registerShortcut("window.tileh", "水平平铺", "Ctrl+Shift+H", SC_Window);
-    registerShortcut("window.tilev", "垂直平铺", "Ctrl+Shift+V", SC_Window);
-    registerShortcut("window.close", "关闭", "Ctrl+F4", SC_Window);
-    registerShortcut("window.closeall", "全部关闭", "Ctrl+Shift+F4", SC_Window);
+    registerShortcut("window.cascade", "层叠", QKeySequence("Ctrl+Shift+C"), SC_Window);
+    registerShortcut("window.tileh", "水平平铺", QKeySequence("Ctrl+Shift+H"), SC_Window);
+    registerShortcut("window.tilev", "垂直平铺", QKeySequence("Ctrl+Shift+V"), SC_Window);
+    registerShortcut("window.close", "关闭", QKeySequence("Ctrl+F4"), SC_Window);
+    registerShortcut("window.closeall", "全部关闭", QKeySequence("Ctrl+Shift+F4"), SC_Window);
 
     // 帮助
     registerShortcut("help.help", "帮助", QKeySequence::HelpContents, SC_Help);
-    registerShortcut("help.about", "关于", "ABOUT", SC_Help);
+    registerShortcut("help.about", "关于", QKeySequence("ABOUT"), SC_Help);
 }
 
 void ShortcutManager::registerShortcut(const QString &id, const QString &name,
